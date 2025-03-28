c03f76e78ecc959d30a2af9679094df6ae604adf
LuanGCMenezes <luangui2006@gmail.com>
2025-03-28 15:54:57 -0300

    Adicionando Cebolinha

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+string fala;
+string falaTrocada;
+
+Console.Write("Digite uma frase: ");
+fala = Console.ReadLine()!.ToLower();
+
+falaTrocada = fala.Replace("r", "l");
+
+Console. WriteLine($"Frase original: {fala}");
+Console. WriteLine($"Frase do Cebolinha: {falaTrocada}");
